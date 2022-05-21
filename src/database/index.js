c4dd1554bee079ceda35cea63258ae38d4126774
Lucas <lucas_gs_lima@hotmail.com>
2022-05-21 15:40:07 -0300

    Updating database folder

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,11 @@
+const { user } = require('pg/lib/defaults')
 const Sequelize = require('sequelize')
 const dbconfig = require('../config/database')
 
+const User = require('../model/User')
+
 const connection = new Sequelize(dbconfig)
+
+User.init(connection)
 
 module.exports = connection
