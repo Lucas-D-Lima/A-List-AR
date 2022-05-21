2f8fb2edcd06ce080a37ccc03c5d37d03b752a77
Lucas <lucas_gs_lima@hotmail.com>
2022-05-21 15:41:25 -0300

    Adding some confings for database

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,11 @@
 module.exports  = {
     dialect : 'postgres',
-    host : 'localhost',
-    username : 'postgres',
-    password : '0000',
-    database : 'CrudTest',
+    username : "postgres",
+    host : "localhost",
+    password : "0000",
+    database : "CrudTest",
     define : {
         timestamps : true,
+        underscored: true,
     }
 }
