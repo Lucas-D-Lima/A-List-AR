1d091fddf214ed6236b203052d6b539934bd77c3
Lucas <lucas_gs_lima@hotmail.com>
2022-05-22 08:49:18 -0300

    Adding dotenv

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,11 @@
+require('dotenv').config()
+
 module.exports  = {
     dialect : 'postgres',
-    username : "postgres",
-    host : "localhost",
-    password : "0000",
-    database : "CrudTest",
+    username : process.env.DB_USERNAME,
+    host : process.env.DB_HOST,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB_DATABASE,
     define : {
         timestamps : true,
         underscored: true,
